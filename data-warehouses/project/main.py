58218d9693d8aebf160dc1c03be4b8bc15bf2743
Natan Nascimento <natanascimentom@outlook.com>
2021-07-10 19:05:45 -0300

    feat: main to process all

--- data-warehouses/project/main.py
+++ data-warehouses/project/main.py
@@ -1,8 +1,9 @@
 from config import Config
+from pipe import ETL, DatabaseManager
 
 if __name__ == '__main__':
-  config = Config()
-  print(config.log_data)
-  print(config.log_path)
-  print(config.song_data)
-  print(config.iam_role)
+  #Startup database tables
+  database_manager = DatabaseManager()
+  database_manager.process()
+  etl = ETL()
+  etl.process()
