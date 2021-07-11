3b6286afdad1dcff90bd0ce5342bff63e07eb016
Natan Nascimento O Matos <natan@teltecsolutions.com.br>
2021-07-11 12:18:05 -0300

    chore: renaming data modeling module

--- data-warehouses/README.md
+++
@@ -1,8 +0,0 @@
-# Data Warehouses Course
-
-This course will be aborded with three submodules:
-
-1. Introduction to Data Warehouses;
-2. Introduction to the Cloud and AWS;
-3. Implementing Data Warehouses on AWS;
-
--- data-warehouses/project/main.py
+++
@@ -1,9 +0,0 @@
-from config import Config
-from pipe import ETL, DatabaseManager
-
-if __name__ == '__main__':
-  #Startup database tables
-  database_manager = DatabaseManager()
-  database_manager.process()
-  etl = ETL()
-  etl.process()
