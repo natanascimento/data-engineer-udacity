798e629f5bc56bdccc09055f19e2259980824634
Natan Nascimento <natanascimentom@outlook.com>
2021-11-08 23:39:21 -0300

    fix: remove dummy dag and financial collect dag

--- 5-capstone/airflow/dags/dummy_dag.py
+++
@@ -1,6 +0,0 @@
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
-
-with DAG('example_dag', start_date=datetime.now()) as dag:
-    op = DummyOperator(task_id='testando_123')
